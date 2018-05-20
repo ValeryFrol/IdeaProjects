55765fa00e82d057a1283ad482b1e57cab190665
Valeriya <vostvalery@gmail.com>
2018-05-20 15:38:18 +0300

    WeightCalc

---
+++ src/main/java/Weight.java
@@ -0,0 +1,13 @@
+public class Weight {
+
+    public double idealMan(double height){
+        double idealWeightMan = (height-100)*1.15;
+        return idealWeightMan;
+    }
+
+    public double idealWoman(double height){
+        double idealWeightWoman = (height-110)*1.15;
+        return idealWeightWoman;
+    }
+
+}
---
+++ src/main/java/WeightCalc.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class WeightCalc {
+
+    public static void main(String[] args) {
+        System.out.println("Male or Female? Press 1 for Male, press 2 for Female");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println("Enter your height");
+        double height = sc.nextInt();
+        Weight w = new Weight();
+
+        if(i==1){
+
+            System.out.println("Your ideal weight - "+w.idealMan(height));
+        }
+        else{
+
+            System.out.println("Your ideal weight - "+w.idealWoman(height));
+        }
+
+
+
+    }
+
+
+
+}

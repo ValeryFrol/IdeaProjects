50905979acec54379f447919fd0f90fbed6c22d0
Valeriya <vostvalery@gmail.com>
2018-05-20 17:46:55 +0300

    Test

--- src/main/java/Weight.java
+++ src/main/java/Weight.java
@@ -1,12 +1,12 @@
 public class Weight {
 
-    public double idealMan(double height){
-        double idealWeightMan = (height-100)*1.15;
+    public double idealMan(double height) {
+        double idealWeightMan = (height - 100) * 1.15;
         return idealWeightMan;
     }
 
-    public double idealWoman(double height){
-        double idealWeightWoman = (height-110)*1.15;
+    public double idealWoman(double height) {
+        double idealWeightWoman = (height - 110) * 1.15;
         return idealWeightWoman;
     }
 
--- src/test/java/WeightTest.java
+++ src/test/java/WeightTest.java
@@ -1,19 +1,20 @@
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 
 public class WeightTest {
-@Test
-public void echo(){
-    double inputHeight = 170;
-    int inputGender = 2;
-    double expectMan = (inputHeight - 100)*1.15;
-    double expectWoman = (inputHeight - 110)*1.15;
-    Weight weight = new Weight();
-    double resultWoman = weight.idealWoman(inputHeight);
-    assertThat(resultWoman,is(expectWoman));
+    @Test
+    public void echo() {
+        double inputHeight = 170;
+        int inputGender = 2;
+        double expectMan = (inputHeight - 100) * 1.15;
+        double expectWoman = (inputHeight - 110) * 1.15;
+        Weight weight = new Weight();
+        double resultWoman = weight.idealWoman(inputHeight);
+        assertThat(resultWoman, is(expectWoman));
 
-}
+    }
 
 }
